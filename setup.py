6780bff646b5b7dd0eca701b8ef1eeda9915baba
Quade Butler <qbutler@uoguelph.ca>
2020-08-15 21:44:12 -0400

    Fixed setup script.

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
-import env
 
 setup(name='ShowMeYourMoney',
-      version=env.VERSION,
+      version="1.40",
       description='A CLI based money management application for scotiabank',
       author='Christian Boin',
       author_email='cboin1996@gmail.com',
       packages=find_packages(),
       install_requires=['pandas', 'numpy', 'matplotlib', 'pyautogui'],
-     )
+      )
