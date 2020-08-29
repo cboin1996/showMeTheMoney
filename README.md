7481a72cc80dd97a4e997511c343d1e5c6eb104b
Jordan Sidsworth <JordansMac@Jordans-MacBook-Pro.local>
2020-08-29 13:47:30 -0400

    updated version nums

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# showMeTheMoney v1.40a
+# showMeTheMoney v1.5
 An CLI app used for inputting banking data and plotting graphs for budgeting purposes.
 
 ### Dependencies
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ShowMeYourMoney',
-      version="1.40a",
+      version="1.5",
       description='A CLI based money management application for scotiabank',
       author='Christian Boin',
       author_email='cboin1996@gmail.com',
